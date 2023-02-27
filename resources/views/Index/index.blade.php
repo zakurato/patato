5257f50021c14a32ed3edf1f3f6687c9dc8041ce
zakurato <stiven_zeledon@hotmail.com>
2023-02-27 14:52:27 -0600

    cambios

--- resources/views/Index/index.blade.php
+++ resources/views/Index/index.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <div class="container">
-		<h1>Patato2</h1>
+		<h1>Patat2o</h1>
 		<form action="{{route("verificarLogin")}}" method="GET">
 			@csrf
 			<label for="username">Correo:</label>
